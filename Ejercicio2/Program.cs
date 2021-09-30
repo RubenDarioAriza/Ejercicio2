5583e4de610537ef19df86737069c84d26e62877
RubenDarioAriza <89140491+RubenDarioAriza@users.noreply.github.com>
2021-09-30 14:36:50 -0500

    Add files via upload

---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    class Ejercicio2
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            int edad;
+            char sexo;
+            double pulsaciones;
+            Console.Write("Digite su nombre: ");
+            nombre = Console.ReadLine();
+            Console.Write("Digite su edad: ");
+            edad = int.Parse(Console.ReadLine());
+            Console.Write("Digite su sexo (M/F): ");
+            sexo = char.Parse(Console.ReadLine());
+            if (sexo.Equals("F"))
+            {
+                pulsaciones = (220 - edad) / 10;
+            }
+            else
+            {
+                pulsaciones = (210 - edad) / 10;
+            }
+            Console.Write("Sus pulsaciones son: " + pulsaciones);
+        }
+    }
+}
+        
+    
+
